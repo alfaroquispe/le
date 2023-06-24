73e89a9b5da531cd9b47e71c98d134b02c2a1a41
alfaro quispe <alfaroquispe@gmail.com>
2023-06-24 12:03:00 -0400

    add for presentation added dulces add pasta larga

--- productos/gallery.js
+++ productos/gallery.js
@@ -5,13 +5,25 @@
 var modal = document.getElementById("product__modal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("expandedImg");
+var img1 = document.getElementById("expandedImg-product1");
+var img2 = document.getElementById("expandedImg-product2");
+var img3 = document.getElementById("expandedImg-product3");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
+// var captionText = document.getElementById("caption");
+img1.onclick = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  // captionText.innerHTML = this.alt;
+}
+img2.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  // captionText.innerHTML = this.alt;
+}
+img3.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  // captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
